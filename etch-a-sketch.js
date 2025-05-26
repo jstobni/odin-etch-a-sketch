cbe4f5a49d1eb939c3731c592a30c3ea8f0e0eb3
Jacob <stobnicki.jake@gmail.com>
2025-05-26 18:31:14 -0400

    Added function to remove the grid and to resize the grid. Defaults to 16x16 grid on page load. Began styling the page. Looking to implement a bar on the left to contain all elements on the page, and have the grid in the middle.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Default to a 16 x 16 grid
     makeGrid(16);
+
+    // Allow resizing of the grid through the form
+    let resizeForm = document.querySelector('#resize');
+    resizeForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        let newSize = document.querySelector('#size').value;
+        removeGrid();
+        makeGrid(newSize)
+    });
 });
 
 function makeGrid(size) {
-    let grid = document.querySelector('#grid')
+    const grid = document.querySelector('#grid')
+    
+    // Calculate square size (with 2px total border)
+    let squareSize = (680 - (size * 2)) / size;
+
     // Place as many rows as the size says
     for (let col = 0; col < size; col++) {
         for (let row = 0; row < size; row++) {
-            grid.innerHTML += "<div class=\"row\"></div>"
+            const square = document.createElement('div');
+            square.className = 'row';
+            square.style = `flex: 1 1 ${squareSize}px;`
+            square.addEventListener('mouseenter', function(event) {
+                event.target.style.backgroundColor = 'black'
+            });
+            grid.appendChild(square)
         }
     }
 }
 
+function removeGrid() {
+    const grid = document.querySelector('#grid');
+    grid.innerHTML = "";
+}
